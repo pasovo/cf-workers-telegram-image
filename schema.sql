fc8f29343cadb82e501b05848b585b908364177a
pasovo <415187499@qq.com>
2025-07-23 22:03:35 +0800

    修改sql

--- schema.sql
+++ schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS images (
   tags TEXT,
   filename TEXT,
   size INTEGER,
-  visit_count INTEGER DEFAULT 0,
+  hash TEXT,
+  folder TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
